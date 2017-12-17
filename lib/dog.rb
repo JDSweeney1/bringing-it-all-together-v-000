8c45049d97e613c81c8690b69b9864119f722aea
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-17 23:50:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,23 +31,25 @@ class Dog
   #   dog
   # end
 
-  def self.find_by_id(id)
-    sql = <<-SQL
-      SELECT * FROM students
-      WHERE id = ?
-    SQL
-
-    data = DB[:conn].execute(sql, id)
-    Dog.new(id: data[0], name: data[1], breed: data[2])
-    Dog
-  end
-  # def save
+  # def self.find_by_id(id)
   #   sql = <<-SQL
-  #     INSERT INTO dogs (name, breed) VALUES (?, ?)
+  #     SELECT * FROM students
+  #     WHERE id = ?
   #   SQL
   #
-  #   DB[:conn].execute(sql, self.name, self.breed)
-  #   self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  #   data = DB[:conn].execute(sql, id)
+  #   Dog.new(id: data[0], name: data[1], breed: data[2])
   #   Dog
   # end
+  def save
+    # if self.idea
+    #
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed) VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    Dog
+  end
 end
